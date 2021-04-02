65b8782b475f1dac1e99f5e5def1f0ad47ce1d92
BettyHoPro <bettyvisualtech@gmail.com>
2021-04-02 08:05:06 +0000

    feat: set up admin auth security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  include HttpAuthConcern
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
   private
+  
+  class PostsController < ApplicationController
+    # http_basic_authenticate_with name:  "Jungle", password: "book", except: :index
+ 
+    def index
+      render plain: "Everyone can see me!"
+    end
+ 
+    def edit
+      render plain: "I'm only accessible if you know the password"
+    end
+    end
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,13 @@
+module HttpAuthConcern  
+    extend ActiveSupport::Concern
+    included do
+        before_action :http_authenticate
+    end
+    def http_authenticate
+        return true unless Rails.env == 'production'
+        authenticate_or_request_with_http_basic do |username, password|
+            sha256_password = Digest::SHA256.hexdigest(password)
+            username == ENV[ADMIN_USERNAME] && password == ENV[ADMIN_PASSWORD]
+        end
+    end
+end
